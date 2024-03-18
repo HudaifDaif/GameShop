8b2c296e3d39abc797a0d4e263ddef3bebdb7a06
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-03-18 21:13:43 +0000

    add DbContext for GameShop and add sqlite to services

---
+++ GameShop.API/Data/GameShopContext.cs
@@ -0,0 +1,10 @@
+﻿using GameShop.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.API.Data;
+
+public class GameShopContext(DbContextOptions<GameShopContext> options) : DbContext(options)
+{
+    public DbSet<Game> Games => Set<Game>();
+    public DbSet<Genre> Genres => Set<Genre>();
+}
--- GameShop.API/Program.cs
+++ GameShop.API/Program.cs
@@ -1,6 +1,11 @@
+using GameShop.API.Data;
 using GameShop.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+string? connectionString = builder.Configuration.GetConnectionString("GameShopDB");
+builder.Services.AddSqlite<GameShopContext>(connectionString);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
