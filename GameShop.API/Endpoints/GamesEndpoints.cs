64298751c47356e2a6319e5660afb2b0ab206364
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-03-24 20:44:18 +0000

    update delete game by id to use database and remove list in memory

--- GameShop.API/Endpoints/GamesEndpoints.cs
+++ GameShop.API/Endpoints/GamesEndpoints.cs
@@ -8,52 +8,6 @@ namespace GameShop.API.Endpoints;
 
 public static class GamesEndpoints
 {
-    private static List<GameSummaryDTO> gameList =
-    [
-        new(
-            Id: 1,
-            Name: "Elden Ring",
-            Genre: "RPG",
-            Price: 59.99m,
-            ReleaseDate: new DateOnly(2022, 2, 25)
-        ),
-        new(
-            Id: 2,
-            Name: "Helldivers 2",
-            Genre: "Third Person Shooter",
-            Price: 34.99m,
-            ReleaseDate: new DateOnly(2024, 2, 8)
-        ),
-        new(
-            Id: 3,
-            Name: "Armored Core VI: Fires of Rubicon",
-            Genre: "Mech",
-            Price: 44.99m,
-            ReleaseDate: new DateOnly(2023, 8, 24)
-        ),
-        new(
-            Id: 4,
-            Name: "Cuphead",
-            Genre: "Platformer",
-            Price: 19.99m,
-            ReleaseDate: new DateOnly(2017, 9, 29)
-        ),
-        new(
-            Id: 5,
-            Name: "Dragon's Dogma 2",
-            Genre: "RPG",
-            Price: 69.99m,
-            ReleaseDate: new DateOnly(2024, 3, 22)
-        ),
-        new(
-            Id: 6,
-            Name: "Palworld",
-            Genre: "Adventure",
-            Price: 29.99m,
-            ReleaseDate: new DateOnly(2024, 1, 19)
-        )
-    ];
-
     public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
         RouteGroupBuilder group = app.MapGroup("/api/games");
@@ -126,9 +80,10 @@ public static class GamesEndpoints
 
         group.MapDelete(
             "/{id}",
-            (int id) =>
+            (int id, GameShopContext dbContext) =>
             {
-                gameList.RemoveAll(game => game.Id == id);
+                dbContext.Games.Where(game => game.Id == id).ExecuteDelete();
+
                 return Results.NoContent();
             }
         );
