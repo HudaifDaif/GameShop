646fba57760c28fa0c2b40122e468c23e4dddd1c
HudaifDaif <107198955+HudaifDaif@users.noreply.github.com>
2024-03-30 17:45:00 +0000

    add client side validation to add game page and swap genre input from text to select element

--- GameShop.Frontend/Models/Game.cs
+++ GameShop.Frontend/Models/Game.cs
@@ -1,10 +1,22 @@
-﻿namespace GameShop.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GameShop.Frontend.Models;
 
 public class Game
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Genre { get; set; }
+
+    [Required]
+    [Range(0, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
